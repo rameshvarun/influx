4eed98d46f87f00353cd87ff819518d612cf6ada
louis <lauises@gmail.com>
2014-01-11 02:17:28 -0800

    UI

---
+++ js/script.js
@@ -0,0 +1,119 @@
+sCount = 0;
+sLeft = $(window).width()*0.05;
+oCount = 0;
+oLeft = $(window).width()*0.05;
+vCount = 0;
+vLeft = $(window).width()*0.05;
+// Source
+$("#newSource").click(function() {
+	$( "#newSourceDialog" ).dialog({
+		width: 250,
+		dialogClass: "no-close",
+		position: { my: "left top", at: "right+50 top-20", of: $("#newSource")},
+		buttons: [
+		{
+			text: "OK",
+			click: function() {
+				$( this ).dialog( "close" );
+				newSource();
+			}
+		}
+		]
+	});
+	$( "#newSourceDialog" ).dialog( "open" );
+})
+
+function newSource() {
+	sCount += 1;
+	$("#workArea").prepend('<div id="s' + sCount+ '" count="'+ sCount +'"class="new objButton source" style="left: ' + sLeft + 'px">Source' + sCount + '</div><div id="sourceDialog' + sCount + '" class="dialog"><form><label for="sourceName">Source Name</label><input type="text" label="sourceName"></form></div>')
+	sLeft += 140;
+	if (sCount == 3) {
+		$("#newSource").hide();
+	};
+}
+
+
+// Operator
+$("#newOperator").click(function() {
+	$( "#newOperatorDialog" ).dialog({
+		width: 250,
+		dialogClass: "no-close",
+		position: { my: "left top", at: "right+50 top-20", of: $("#newOperator")},
+		buttons: [
+		{
+			text: "OK",
+			click: function() {
+				$( this ).dialog( "close" );
+				newOperator();
+			}
+		}
+		]
+	});
+	$( "#newOperatorDialog" ).dialog( "open" );
+})
+function newOperator() {
+	oCount += 1;
+	$("#workArea").prepend('<div id=o"' + oCount+ '" class="new objButton operator" style="left: ' + oLeft + 'px">Operator' + oCount + '</div>')
+	oLeft += 140;
+	if (oCount == 3) {
+		$("#newOperator").hide();
+	};
+}
+
+// Viz
+$("#newViz").click(function() {
+	$( "#newVizDialog" ).dialog({
+		width: 250,
+		dialogClass: "no-close",
+		position: { my: "left top", at: "right+50 top-20", of: $("#newViz")},
+		buttons: [
+		{
+			text: "OK",
+			click: function() {
+				$( this ).dialog( "close" );
+				newViz();
+			}
+		}
+		]
+	});
+	$( "#newVizDialog" ).dialog( "open" );
+})
+function newViz() {
+	vCount += 1;
+	$("#workArea").prepend('<div id=v"' + vCount+ '" class="new objButton viz" style="left: ' + vLeft + 'px">Viz' + vCount + '</div>')
+	vLeft += 140;
+	if (vCount == 3) {
+		$("#newViz").hide();
+	};
+}
+
+// Set Attributes
+$(".new").click(function(){
+	console.log($(this).attr("count"));
+	var count;
+	$( "#sourceDialog" + count).dialog({
+		width: 250,
+		dialogClass: "no-close",
+		position: { my: "left top", at: "right+50 top-20", of: $("#s" + count)},
+		buttons: [
+		{
+			text: "OK",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+		]
+	});
+	$( "#sourceDialog" + count).dialog( "open" );
+})
+
+
+
+
+
+
+
+
+
+
+
